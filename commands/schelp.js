77c302440b4a9a350adbb53a073bebaffe58fc07
Demonkillua <80291747+Demonkillua@users.noreply.github.com>
2021-03-08 21:12:41 -0500

    Removed image from schelp
    Image command not working in VPS. Browser may be needed for command to execute.

--- commands/schelp.js
+++ commands/schelp.js
@@ -4,6 +4,6 @@ module.exports = {
     permissions: [],
     description: "displays current shortcuts!",
     execute(message, args, cmd, client, Discord) {
-        message.channel.send(`**Command shortcuts list**\n\nCurrent shortcuts are as follows;\n\n${prefix}image *keywords* - ${prefix}img *keywords*\n${prefix}roll # - ${prefix}r #\n${prefix}balance - ${prefix}bal **or** ${prefix}bl\n${prefix}deposit # - ${prefix}dep #\n${prefix}withdraw # - ${prefix}wd #\n\n**Moderator Commands**\n${prefix}clear # - ${prefix}delete # **or** ${prefix}cl #\n${prefix}give @user # - ${prefix}gift @user #`);
+        message.channel.send(`**Command shortcuts list**\n\nCurrent shortcuts are as follows;\n\n${prefix}roll # - ${prefix}r #\n${prefix}balance - ${prefix}bal **or** ${prefix}bl\n${prefix}deposit # - ${prefix}dep #\n${prefix}withdraw # - ${prefix}wd #\n\n**Moderator Commands**\n${prefix}clear # - ${prefix}delete # **or** ${prefix}cl #\n${prefix}give @user # - ${prefix}gift @user #`);
     }
 }
