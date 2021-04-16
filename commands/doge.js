8bb05e9778804720c42d946e53a3ffa481270cba
Demonkillua <80291747+Demonkillua@users.noreply.github.com>
2021-04-16 19:35:56 -0400

    Delete doge.js

--- commands/doge.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//     name: 'doge',
-//     permissions: [],
-//     description: "dogecoin ticker!",
-//     execute(message, args, cmd, client, Discord) {
-//         message.channel.send('ticker not yet available');
-//     }
-// }
