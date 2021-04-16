a3ba79a782c05b4badb22d6d9d7bfd13e7e12bd0
Demonkillua <80291747+Demonkillua@users.noreply.github.com>
2021-04-16 19:36:03 -0400

    Delete elon.js

--- commands/elon.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//     name: 'elon',
-//     permissions: [],
-//     description: "sends twitter link!",
-//     execute(message, args, cmd, client, Discord) {
-//         message.channel.send('http://www.twitter.com/elonmusk');
-//     }
-// }
